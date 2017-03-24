6b4e6c923fac25242031693778452c80e76a8c94
Jack Kaloger <jkaloger@users.noreply.github.com>
2017-03-24 00:25:25 +0000

    initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+/* Memory Management program
+ * Project 1 COMP30023
+ * Jack Kaloger 2017
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "swap.h"
+
+int main(int argc, char **argv, char **envp) {
+
+    return 0;
+}

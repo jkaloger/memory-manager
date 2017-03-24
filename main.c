859f84b2f18d075cbfa6a83f6cc6e5d1ce1bfc1b
Jack Kaloger <jkaloger@users.noreply.github.com>
2017-03-24 04:01:19 +0000

    added file parser

--- main.c
+++ main.c
@@ -5,11 +5,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 #include "swap.h"
 
-int main(int argc, char **argv, char **envp) {
+void parse(char *file, int ***A);
 
+int main(int argc, char **argv)
+{
+    int **table;
+    table = malloc(sizeof(int *) * 4);
+    parse(argv[1], &table);
+    
     return 0;
+}
+
+void parse(char *file, int ***A)
+{
+    // Read file
+    FILE *f;
+    f = fopen(file, "r");
+    if(f == NULL) {
+        fprintf(stderr, "Error opening file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // initialise
+    for(int j = 0 ; j < 4 ; j++)
+        (*A)[j] = malloc(sizeof(int));
+    int i = 0;
+    while(fscanf(f, "%d %d %d %d", &(*A)[0][i], &(*A)[1][i], &(*A)[2][i], &(*A)[3][i]) == 4) {
+        i++;
+        for(int j = 0 ; j < 4 ; j++) {
+            (*A)[j] = realloc((*A)[j], sizeof(int) * i+1);
+        }
+    }
 }
---
+++ swap.c
@@ -0,0 +1,10 @@
+/* Swap.c
+ * Swap functions for COMP30023 Project 1
+ * Jack Kaloger 2017
+ */
+
+void bestFirst()
+{
+
+
+}
